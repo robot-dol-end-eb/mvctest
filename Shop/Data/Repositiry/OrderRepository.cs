1d13074c3ab15698319aba0b1ebbd55f90b82579
robot-dol-and-eb <bikoluso@gmail.com>
2020-03-27 19:12:26 +0300

    add order

---
+++ Shop/Data/Repositiry/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Repositiry
+{
+    public class OrderRepository : IAllOrders
+    {
+        private readonly AppDBContent appDBContent;
+        private readonly ShopCart shopCart;
+        public OrderRepository(AppDBContent appDBContent, ShopCart shopCart)
+        {
+            this.appDBContent = appDBContent;
+            this.shopCart = shopCart;
+        }
+        public void createOrder(Order order)
+        {
+            order.OrederTime = DateTime.Now;
+            //appDBContent.
+        }
+    }
+}
